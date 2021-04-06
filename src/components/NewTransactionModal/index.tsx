cd85f7f44760591a3810bef0c01cf68163344bb1
Marcelo Soares <mamsoares@gmail.com>
2021-04-05 22:13:29 -0300

    Create app structure and components

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, RadioBox, TransationTypeContainer } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal( { isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
+  return (
+    <Modal 
+      isOpen={isOpen} 
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type="button" 
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input 
+          placeholder="Título" 
+        />
+
+        <input 
+          type="number" 
+          placeholder="Valor" 
+        />
+
+        <TransationTypeContainer>
+          <RadioBox 
+            type="button"
+            onClick={() => { setType('deposit'); }}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entradas" />
+            <span>Entradas</span>
+          </RadioBox>
+        
+          <RadioBox
+            type="button" 
+            onClick={() => { setType('withdraw'); }}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saídas" />
+            <span>Saídas</span>
+          </RadioBox>
+          
+        </TransationTypeContainer>
+
+        <input 
+          placeholder="Categoria" 
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+      </Container>
+
+      <form>
+
+      </form>
+
+
+    </Modal>
+
+  )
+}
